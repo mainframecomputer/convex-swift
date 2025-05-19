26a6e777198a22074bfd6e2d3a374894976e5a4c
Tomás Martins <tomfmart@gmail.com>
2025-05-19 13:25:13 -0300

    Whitespace formatting

--- Sources/ConvexMobile/ConvexMobile.swift
+++ Sources/ConvexMobile/ConvexMobile.swift
@@ -285,11 +285,11 @@ private class SubscriptionAdapter<T: Decodable>: QuerySubscriber {
   }
 
   func onUpdate(value: String) {
-      do {
-          let decodedData = try JSONDecoder().decode(Publisher.Output.self, from: Data(value.utf8))
-          publisher.send(decodedData)
-      } catch {
-          publisher.send(completion: Subscribers.Completion.failure(ClientError.InternalError(msg: error.localizedDescription)))
-      }
+    do {
+      let decodedData = try JSONDecoder().decode(Publisher.Output.self, from: Data(value.utf8))
+      publisher.send(decodedData)
+    } catch {
+      publisher.send(completion: Subscribers.Completion.failure(ClientError.InternalError(msg: error.localizedDescription)))
+    }
   }
 }
